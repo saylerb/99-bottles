c85ac220527333e99637450e88c6b8a164ae12d0
Brian Sayler <sayler.b@gmail.com>
2017-02-20 18:24:06 -0700

    add pluralize bottles method

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,7 +1,24 @@
 class Bottles
+
   def verse(number)
-    %(#{number} bottles of beer on the wall, #{number} bottles of beer.
-Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+    %(#{get_bottles(number)} on the wall, #{get_bottles(number)}.
+Take #{number == 1 ? "it" : "one"} down and pass it around, #{get_bottles(number - 1)} on the wall.
 )
   end
+
+  def get_bottles(number)
+    "#{number} #{pluralize_bottle(number)} of beer"
+  end
+
+  def pluralize_bottle(number)
+    case number
+    when 0
+      "no more bottles"
+    when 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
 end
