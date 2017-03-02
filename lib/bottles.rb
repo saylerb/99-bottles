3eff66030ece67ee45055b8b06f98c71374bf789
Brian Sayler <sayler.b@gmail.com>
2017-03-01 23:58:35 -0700

    add correct 2nd line for the 0th verse

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,14 +1,23 @@
 class Bottles
 
   def verse(number)
-    %(#{line_one(number)}
-Take #{it_or_one(number)} down and pass it around, #{get_bottles(number - 1)} on the wall.
+    %(#{line_one(number).capitalize}
+#{line_two(number)}
 )
   end
 
   def line_one(number)
     get_bottles(number) +
       " on the wall, #{get_bottles(number)}."
+  end
+
+  def line_two(number)
+    case number
+    when 0
+      "Go to the store and buy some more, 99 bottles of beer on the wall."
+    else
+      "Take #{it_or_one(number)} down and pass it around, #{get_bottles(number - 1)} on the wall."
+    end
   end
 
   def get_bottles(number)
